19a07bce5f90896a932021c3258dd37ddb97b981
shahabhusain <shahabhussain098123@gmail.com>
2025-07-28 15:10:20 +0500

    chnages

--- src/components/home/Build.jsx
+++ src/components/home/Build.jsx
@@ -5,7 +5,7 @@ const Build = () => {
   return (
     <div className=' flex flex-col items-center justify-center gap-y-4 h-[470px] w-full'>
       <h1 className=' md:text-[56px] text-[40px] font-[800] text-white max-w-[775px] text-center'><span className=' font-[400]'>Let’s Build</span> Something <span className=' bg-gradient-to-l from-red-500 via-orange-500 to-pink-500 bg-clip-text text-transparent'>Great Together!</span> </h1>
-      <Link to="/" className=' py-3 px-6 rounded-md bg-white text-[14px] font-[600] text-black'>SECURE A CODE</Link>
+      <Link to="/" className=' py-3 px-6 rounded-md bg-white text-[14px] font-[600] text-black'>SECURE A QUOTE</Link>
     </div>
   )
 }
